2271d720451f099147a8fc51b705c0587dc55257
np646 <natalysalazar646@gmail.com>
2024-10-13 20:54:53 -0500

    Updates statistics layout

--- resources/js/Pages/Statistics/Index.vue
+++ resources/js/Pages/Statistics/Index.vue
@@ -2,54 +2,186 @@
     <MenuLayout>
         <Title :title="title" />
         <ContentContainer>
-            <div class="m-10">
-                <Chart type="line" :data="lineData" :options="lineOptions" class="h-[20rem]" />
-            </div>
-
-            <div class="d-flex justify-content-center mb-10">
+            <div class="pb-3 pt-2 pl-3">
                 <div class="row">
-                    <label for="inputSelectPeriodo" class="col-form-label text-center">Periodo</label>
-                    <div class="row">
-                        <Select
-                            class="w-100"
-                            id="inputSelectPeriodo"
-                            :options="period"
-                            :optionLabel="label"
-                            :placeholder="placeholder"
-                            v-model="selectedOption"
-                        />
+                    <div class="col-9">
+                        <Chart type="line" :data="lineData" :options="lineOptions" class="h-[15rem]" />
+                    </div>
+                    <div class="col h-[15rem] d-flex flex-column justify-content-center">
+                        <div class="h-ful">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <div class="row mb-2">
+                                        <span class="fs-5">Estudiantes</span>
+                                    </div>
+                                    <div class="row">
+                                        <span class="fw-bold fs-2">3050</span>
+                                    </div>
+                                </div>
+                                <div class="col d-flex justify-content-center align-items-center">
+                                    <i class="pi pi-user text-blue-600" style="font-size: 300%"></i>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <div class="row mb-2">
+                                        <span class="fs-5">Graduados</span>
+                                    </div>
+                                    <div class="row">
+                                        <span class="fw-bold fs-2">1450</span>
+                                    </div>
+                                </div>
+                                <div class="col d-flex justify-content-center align-items-center">
+                                    <i class="pi pi-graduation-cap text-blue-600" style="font-size: 300%"></i>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="d-flex justify-content-center text-center items-center mb-10">
-                <div class="col">
-                    Tipo de titulación
-                    <div class="d-flex justify-content-center text-center items-center">
-                        <Chart type="pie" :data="doughnutData" :options="doughnutOptions" class="w-50" />
-                    </div>
-                </div>
-
-                <div class="col">
-                    Línea de investigación
-                    <div class="d-flex justify-content-center text-center items-center">
-                        <Chart type="pie" :data="doughnutData1" :options="doughnutOptions1" class="w-50" />
-                    </div>
-                </div>
+        </ContentContainer>
+        <ContentContainer>
+            <div class="pl-5 pt-3">
+                <span class="font-semibold fs-5">Graduados por periodo académico</span>
             </div>
-
-            <div class="d-flex justify-content-around text-center items-center" style="height: 400px">
+            <div class="d-flex justify-content-around items-center p-3" style="height: 400px">
                 <div class="w-50">
-                    <DataTable :value="graduates" class="w-full">
-                        <Column field="course" header="Carrera" />
-                        <Column field="female" header="Mujeres" />
-                        <Column field="male" header="Hombres" />
-                        <Column field="total" header="Total" />
-                    </DataTable>
+                    <div>
+                        <div class="col">
+                            <div class="flex items-center gap-4 mb-4 pl-4">
+                                <Select
+                                    class="w-100"
+                                    id="inputSelectPeriodo"
+                                    :options="period"
+                                    optionLabel="period"
+                                    :placeholder="`Seleccione uno`"
+                                    v-model="selectedOption"
+                                />
+                            </div>
+                        </div>
+                        <div class="col">
+                            <DataTable :value="graduates" class="w-full">
+                                <Column field="course" header="Carrera" />
+                                <Column field="female" header="Mujeres" />
+                                <Column field="male" header="Hombres" />
+                                <Column field="total" header="Total" />
+                            </DataTable>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="d-flex h-full justify-content-center w-50">
-                    <Chart type="bar" :data="barData" :options="barOptions" class="w-full" />
+                <div class="d-flex h-full w-50 justify-content-center text-center items-center">
+                    <Chart type="pie" :data="doughnutData" :options="doughnutOptions" class="w-75" />
+                </div>
+            </div>
+
+            <div class="pl-5 pt-3">
+                <span class="font-semibold fs-5">Graduados por tipo de titulación</span>
+            </div>
+
+            <div class="pb-3 pt-2 pl-5 pr-5">
+                <div class="row h-[15rem]">
+                    <div class="col d-flex flex-column justify-content-center">
+                        <div class="h-ful">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <div class="row mb-2">
+                                        <span class="fs-6">Examen complexivo</span>
+                                    </div>
+                                    <div class="row">
+                                        <span class="fw-bold fs-3">3050</span>
+                                    </div>
+                                </div>
+                                <div class="col d-flex justify-content-center align-items-center">
+                                    <i class="pi pi-book text-blue-600" style="font-size: 200%"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col d-flex flex-column justify-content-center">
+                        <div class="h-ful">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <div class="row mb-2">
+                                        <span class="fs-6">Proyecto de investigación</span>
+                                    </div>
+                                    <div class="row">
+                                        <span class="fw-bold fs-3">3050</span>
+                                    </div>
+                                </div>
+                                <div class="col d-flex justify-content-center align-items-center">
+                                    <i class="pi pi-paperclip text-blue-600" style="font-size: 200%"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <Chart type="bar" :data="barData" :options="barOptions" class="h-full" />
+                    </div>
+                </div>
+            </div>
+
+            <div class="pl-5 pt-3 pb-3">
+                <span class="font-semibold fs-5">Graduados por línea de investigación</span>
+            </div>
+            <div class="pb-3 pt-2 pl-5 pr-5">
+                <div class="row h-[15rem]">
+                    <div class="col-3 h-full">
+                        <div class="d-flex justify-content-center text-center items-center">
+                            <Chart type="pie" :data="doughnutData1" :options="doughnutOptions1" class="w-full" />
+                        </div>
+                    </div>
+                    <div class="col d-flex flex-column justify-content-center">
+                        <div class="h-ful">
+                            <div class="row mb-3 p-3">
+                                <div class="col">
+                                    <div class="row mb-2">
+                                        <span class="fs-6">Software</span>
+                                    </div>
+                                    <div class="row">
+                                        <span class="fw-bold fs-3">3050</span>
+                                    </div>
+                                </div>
+                                <div class="col d-flex justify-content-center align-items-center">
+                                    <i class="pi pi-desktop text-blue-600" style="font-size: 200%"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col d-flex flex-column justify-content-center">
+                        <div class="h-ful">
+                            <div class="row mb-3 p-3">
+                                <div class="col">
+                                    <div class="row mb-2">
+                                        <span class="fs-6">IoT</span>
+                                    </div>
+                                    <div class="row">
+                                        <span class="fw-bold fs-3">3050</span>
+                                    </div>
+                                </div>
+                                <div class="col d-flex justify-content-center align-items-center">
+                                    <i class="pi pi-microchip text-blue-600" style="font-size: 200%"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col d-flex flex-column justify-content-center">
+                        <div class="h-ful">
+                            <div class="row mb-3 p-3">
+                                <div class="col">
+                                    <div class="row mb-2">
+                                        <span class="fs-6">Auditoría informática</span>
+                                    </div>
+                                    <div class="row">
+                                        <span class="fw-bold fs-3">3050</span>
+                                    </div>
+                                </div>
+                                <div class="col d-flex justify-content-center align-items-center">
+                                    <i class="pi pi-file-check text-blue-600" style="font-size: 200%"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </ContentContainer>
@@ -116,9 +248,7 @@ const setlineOptions = () => {
         aspectRatio: 0.6,
         plugins: {
             legend: {
-                labels: {
-                    color: textColor,
-                },
+                display: false,
             },
         },
         scales: {
@@ -149,8 +279,6 @@ const period = ref([
 ]);
 
 const selectedOption = ref(null);
-const label = "period";
-const placeholder = "Seleccione uno";
 
 // Watch for changes in selectedOption value
 watch(selectedOption, () => {
@@ -166,20 +294,12 @@ const setdoughnutData = () => {
     const documentStyle = getComputedStyle(document.body);
 
     return {
-        labels: ["Trabajo de investigación", "Examen complexivo", "C"],
+        labels: ["Hombres", "Mujeres"],
         datasets: [
             {
-                data: [540, 325, 702],
-                backgroundColor: [
-                    documentStyle.getPropertyValue("--p-cyan-500"),
-                    documentStyle.getPropertyValue("--p-orange-500"),
-                    documentStyle.getPropertyValue("--p-gray-500"),
-                ],
-                hoverBackgroundColor: [
-                    documentStyle.getPropertyValue("--p-cyan-400"),
-                    documentStyle.getPropertyValue("--p-orange-400"),
-                    documentStyle.getPropertyValue("--p-gray-400"),
-                ],
+                data: [540, 325],
+                backgroundColor: [documentStyle.getPropertyValue("--p-teal-400"), documentStyle.getPropertyValue("--p-pink-400")],
+                hoverBackgroundColor: [documentStyle.getPropertyValue("--p-cyan-400"), documentStyle.getPropertyValue("--p-orange-400")],
             },
         ],
     };
@@ -249,6 +369,7 @@ const setdoughnutOptions1 = () => {
 const graduates = ref([
     { id: 0, course: "INGENIERÍA EN SISTEMAS", female: "50", male: "50", total: "100" },
     { id: 1, course: "TECNOLOGÍAS DE LA INFORMACIÓN", female: "50", male: "50", total: "100" },
+    { id: 1, course: "INGENIERÍA DE SOFTWARE", female: "50", male: "50", total: "100" },
 ]);
 
 // Gender chart
@@ -260,19 +381,12 @@ const setbarData = () => {
     const documentStyle = getComputedStyle(document.documentElement);
 
     return {
-        labels: ["INGENIERÍA EN SISTEMAS", "TECNOLOGÍAS DE LA INFORMACIÓN", "INGENIERÍA DE SOFTWARE"],
+        labels: ["EXAMEN COMPLEXIVO", "PROYECTO DE TITULACIÓN"],
         datasets: [
             {
-                label: "Mujeres",
                 backgroundColor: documentStyle.getPropertyValue("--p-cyan-500"),
                 borderColor: documentStyle.getPropertyValue("--p-cyan-500"),
-                data: [65, 59, 80],
-            },
-            {
-                label: "Hombres",
-                backgroundColor: documentStyle.getPropertyValue("--p-gray-500"),
-                borderColor: documentStyle.getPropertyValue("--p-gray-500"),
-                data: [28, 48, 40],
+                data: [65, 59],
             },
         ],
     };
@@ -288,9 +402,7 @@ const setbarOptions = () => {
         aspectRatio: 0.8,
         plugins: {
             legend: {
-                labels: {
-                    color: textColor,
-                },
+                display: false,
             },
         },
         scales: {
